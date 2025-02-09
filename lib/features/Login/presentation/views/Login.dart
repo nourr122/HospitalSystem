2761d5e5de745d413c73939ea28a8b225bfdd416
nourr122 <152524706+nourr122@users.noreply.github.com>
2025-02-09 20:17:53 +0200

    Added features folder

---
+++ lib/features/Login/presentation/views/Login.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/app_colors.dart';
+import '../../../../core/utils/assets.dart';
+import '../../../../core/widgets/gradient_container.dart';
+import '../../../prototype/presentation/views/prototype.dart';
+
+class LoginPage extends StatefulWidget {
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+var size, height, width;
+class _LoginScreenState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false, // Prevent automatic resizing
+      body: Stack(
+        children: [
+          const Align(
+            alignment: Alignment.topLeft,
+            child: GradientContainer(
+              height: 280,
+              width: 300,
+              borderRadius: BorderRadius.only(topRight: Radius.circular(100)),
+              colors: [AppColors.gradientColor1, AppColors.gradientColor2],
+            ),
+          ),
+          const Align(
+            alignment: Alignment.bottomRight,
+            child: GradientContainer(
+              height: 250,
+              width: 280,
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(100)),
+              colors: [AppColors.gradientColor1, AppColors.gradientColor2],
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8, // Adjust dynamically
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            AppAssets.logo2,
+                            height: 180,
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Welcome back !',
+                            style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          const Text(
+                            'To Continue, Login Now',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Color(0xfff828282),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          Flexible(
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                prefixIcon: Image.asset(AppAssets.phone, height: 20),
+                                hintText: '  Phone Number',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              keyboardType: TextInputType.phone,
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          Flexible(
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                prefixIcon: Image.asset(AppAssets.lock, height: 50),
+                                suffixIcon: Image.asset(AppAssets.eye, height: 50),
+                                hintText: '  Password',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              keyboardType: TextInputType.visiblePassword,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Forget Password?',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xfff000000),
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 60,
+                      width: 380,
+                      child: ElevatedButton(
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  prototypePage(), // Replace with your target page
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
---
+++ lib/features/Splash/presentation/views/Splash.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/app_colors.dart';
+import '../../../../core/utils/assets.dart';
+import '../../../../core/widgets/gradient_container.dart';
+import '../../../Login/presentation/views/Login.dart';
+
+class Splash extends StatefulWidget{
+  const Splash({super.key, required this.title});
+
+  final String title;
+
+
+  @override
+  State<Splash> createState() => _Splash();
+}
+
+class _Splash extends State<Splash>  with SingleTickerProviderStateMixin{
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  var size,height,width;
+
+
+ @override
+  void initState() {
+   super.initState();
+   _controller = AnimationController(
+     duration: const Duration(seconds: 5),
+     vsync: this,
+   );
+
+   _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
+     ..addListener(() {
+       setState(() {});
+     })
+    ..addStatusListener((status) {
+   if (status == AnimationStatus.completed) {
+     Navigator.pushReplacement(
+       context,
+       MaterialPageRoute(
+         builder: (context) => LoginPage(),
+       ),
+     );
+   }
+   });
+
+   _controller.forward();
+ }
+
+ @override
+ void dispose() {
+   _controller.dispose();
+   super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+   return Scaffold(
+     backgroundColor: Colors.white,
+    body: Stack(
+       children: [
+         const Align(
+            alignment: Alignment.topLeft,
+           child: GradientContainer(
+             height: 280,
+             width: 300,
+             borderRadius: BorderRadius.only(topRight: Radius.circular(100)),
+             colors: [AppColors.gradientColor1, AppColors.gradientColor2],
+           ),
+         ),
+         const Align(
+           alignment: Alignment.bottomRight,
+           child: GradientContainer(
+             height: 250,
+             width: 280,
+             borderRadius: BorderRadius.only(bottomLeft: Radius.circular(100)),
+             colors: [AppColors.gradientColor1, AppColors.gradientColor2],
+           ),
+         ),
+         SizedBox(
+           height: height * 1.1,
+           child: Center(
+             child: Column(
+               mainAxisSize: MainAxisSize.min,
+               children: [
+                 Image.asset(
+                   AppAssets.logo2,
+                   height: 250,
+                 ),
+                 SizedBox(height: height * 0.2),
+                 SizedBox(
+                   width: width * 0.8,
+                   child: LinearProgressIndicator(
+                     value: _animation.value,
+                     backgroundColor: const Color(0xFFF4F6466),
+                     valueColor:
+                     const AlwaysStoppedAnimation(AppColors.primaryColor),
+                     minHeight: 5,
+                   ),
+                 ),
+                 SizedBox(height: 20),
+                 // Loading Text
+                 const Text(
+                   'Loading...',
+                   style: TextStyle(fontSize: 20, color: AppColors.primaryColor, fontWeight: FontWeight.bold),
+                 ),
+
+               ],
+             ),
+           ),
+
+         ),
+
+       ],
+     ),
+   );
+
+  }
+}
+
