4111d1005a9a1c08b4735ecb7093a3adb093eeb0
ShazaElnagar <elnagarshaza06@gmail.com>
2025-02-09 10:24:18 -0800

    new report

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFFFFFF);
+  static const Color accentColor = Color(0xFF4CAF50);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color textColor = Color(0xFF212121);
+  static const Color TitleColor = Color(0xFF343434);
+  static const Color ReportColor = Color(0xFF00000080);
+
+  static var dividerColor;
+
+  static var blueColor;
+}
