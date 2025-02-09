329901ba16101be56fb135ddf4d8cab3c89d023d
wessam23 <wessammagdy23@gmail.com>
2025-02-09 19:23:12 +0200

    Create app_assets.dart

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,35 @@
+
+class AppAssets{
+  static const String add = 'assets/images/add.png';
+  static const String calender = 'assets/images/calender.png';
+  static const String callCalender = 'assets/images/calls calender.png';
+  static const String callsProfile = 'assets/images/calls profile.png';
+  static const String callstime = 'assets/images/calls_time.png';
+  static const String check = 'assets/images/check.png';
+  static const String circleArrow = 'assets/images/circle_arrow.png';
+  static const String download = 'assets/images/download.png';
+  static const String edit = 'assets/images/edit.png';
+  static const String eye = 'assets/images/eye.png';
+  static const String greenChecked = 'assets/images/green_checked.png';
+  static const String lock = 'assets/images/lock.png';
+  static const String logo = 'assets/images/logo.png';
+  static const String logo2 = 'assets/images/logo2.png';
+  static const String phone = 'assets/images/phone.png';
+  static const String profile = 'assets/images/profile.png';
+  static const String undo = 'assets/images/undo.png';
+  static const String wrong = 'assets/images/wrong.png';
+  static const String nurse1 = 'assets/images/nurse1.jpg';
+  static const String nurse2 = 'assets/images/nurse2.jpg';
+  static const String nurse3 = 'assets/images/nurse3.jpg';
+  static const String nurse4 = 'assets/images/nurse4.jpg';
+  static const String nurse5 = 'assets/images/nurse5.jpg';
+  static const String nurse6 = 'assets/images/nurse6.jpg';
+  static const String Aml = 'assets/images/Aml.jpg';
+  static const String path = 'assets/images/Path.png';
+  static const String dashboard = 'assets/images/dashboard.jpg';
+  static const String treatment = 'assets/images/treatment.jpg';
+  static const String uploadImage = 'assets/images/uploadImage.jpg';
+  static const String dot = 'assets/images/dot.jpg';
+
+  
+}
