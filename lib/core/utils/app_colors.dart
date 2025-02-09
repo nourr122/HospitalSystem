7903fe10ee58808d19f8a4b461ae462ebf9f06c1
nourr122 <152524706+nourr122@users.noreply.github.com>
2025-02-09 20:20:43 +0200

    core

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  // blue colors
+  static const Color primaryColor = Color(0xff22C7B8);
+  // gray to black colors
+  static const Color grayColor1 = Color(0xff707070);
+  static const Color grayColor2 = Color(0xff404040);
+  static const Color grayColor3 = Color(0xff7A7A7A);
+
+  static const Color gradientColor1 = Color(0x0122C7B8);
+  static const Color gradientColor2 =  Color(0x60FFFFFF);
+}
