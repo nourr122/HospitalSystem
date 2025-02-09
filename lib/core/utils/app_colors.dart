20c14f200a3c43064184fad5a903123b627fa3f5
wessam23 <wessammagdy23@gmail.com>
2025-02-09 19:23:39 +0200

    Add files via upload

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  // blue colors
+  static const Color primaryColor = Color(0xff22C7B8);
+  static const Color secondColor = Color(0xff2FBEB0);
+  static const Color blueColor = Color(0xffE0F7F5);
+  // gray to black colors
+  static const Color grayColor1 = Color(0xff707070);
+  static const Color grayColor2 = Color(0xff404040);
+  static const Color grayColor3 = Color(0xff7A7A7A);
+  // orange color for alert
+  static const Color orangeColor1 = Color(0xffF36D13);
+  static const Color orangeColor2 = Color.fromARGB(255, 249, 206, 186);
+  // grren colors
+  static const Color greenColor1 = Color(0xFF19D571);
+  //white colors
+  static const Color whiteColor1 = Color(0xFFffffff);
+  static const Color blackColor1 = Color(0xFF000000);
+  static const Color redColor1 = Color(0xffE63A3A);
+
+}
+
