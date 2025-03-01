8fb4334ad4f7792204f28d2d92f62aa79a11c3a2
shazaElnagar <93821268+shazaElnagar@users.noreply.github.com>
2025-03-01 01:26:04 -0800

    Update show_report.dart

--- lib/features/reports/data/models/show_report.dart
+++ lib/features/reports/data/models/show_report.dart
@@ -1,7 +1,7 @@
 class ReportDetail {
   final int status;
   final String message;
-  final Data? data; // استخدام ? لأن البيانات قد تكون غير موجودة
+  final Data? data; 
 
   ReportDetail({required this.status, required this.message, this.data});
 
