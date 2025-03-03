00683fd6ed2627adf60d8f733e4fa105266db0fc
ZeinabAhmed03 <125619131+ZeinabAhmed03@users.noreply.github.com>
2025-03-03 02:51:41 +0200

    Update show_call_details_cubit.dart

--- lib/features/calls/doctor calls/cubits/Show Call Details/show_call_details_cubit.dart
+++ lib/features/calls/doctor calls/cubits/Show Call Details/show_call_details_cubit.dart
@@ -1,3 +1,29 @@
+// import 'dart:developer';
+
+// import 'package:bloc/bloc.dart';
+// import 'package:instant_project/features/calls/doctor%20calls/data/models/call_details_model.dart';
+// import 'package:instant_project/features/calls/doctor%20calls/presentation/services/show_call.dart';
+// import 'package:meta/meta.dart';
+
+// part 'show_call_details_state.dart';
+
+// class ShowCallDetailsCubit extends Cubit<ShowCallDetailsState> {
+//   ShowCallDetailsCubit(this.id, this.token) : super(ShowCallDetailsInitial());
+//   final int id;
+//   final String token;
+//    void getCallDetails() async{
+//   emit(ShowCallDetailsLoading());
+//   try {
+//      CallDetailsModel callsDetails = await showCallsDetails(id: id, token: token);
+//      log('All Details from cubit: ${callsDetails.toString()}');
+//      emit(ShowCallDetailsLoaded(callDetails: callsDetails));
+//    } catch (e) {
+//     print(e);
+//     emit(ShowCallDetailsError());
+//    }
+//   }
+// }
+
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
