a41e35eef85ff05a1b39c20a6a2b27e9b3281d56
ZeinabAhmed03 <125619131+ZeinabAhmed03@users.noreply.github.com>
2025-03-03 02:53:22 +0200

    Update accept_reject_call_service.dart

--- lib/features/calls/doctor calls/presentation/services/accept_reject_call_service.dart
+++ lib/features/calls/doctor calls/presentation/services/accept_reject_call_service.dart
@@ -1,3 +1,45 @@
+// import 'dart:developer';
+
+// import 'package:flutter/material.dart';
+// import 'package:instant_project/features/calls/constants.dart';
+// import 'package:instant_project/features/calls/helper.dart';
+
+// class AcceptRejectCallService {
+//   Future<void> putService({
+//     required String status,
+//     required BuildContext context,
+//     required int id,
+//     required String token
+//   }) async {
+//     final Map<String, dynamic> body = {
+//       'status': status,
+//     };
+
+//     try {
+//       log('Sending Put request: $body');
+//       final response = await Api().put(
+//         url: '$acceptRejectApi/$id',
+//         body: body,
+//         token: token, 
+//         context: context,
+//       );
+
+//       // Display Snackbar with the response message
+//       final String message = response['message'] ?? 'Unknown response';
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text(message)),
+//       );
+
+//       log('Put Successful: $message');
+//     } catch (e) {
+//       log('POST Error: $e');
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text('Request failed: $e')),
+//       );
+//     }
+//   }
+// }
+
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
