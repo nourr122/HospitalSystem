37bc6d39b9b62ab2a55564ef480f1a08310673de
ZeinabAhmed03 <125619131+ZeinabAhmed03@users.noreply.github.com>
2025-03-03 02:54:43 +0200

    Update show_call.dart

--- lib/features/calls/doctor calls/presentation/services/show_call.dart
+++ lib/features/calls/doctor calls/presentation/services/show_call.dart
@@ -1,3 +1,28 @@
+// import 'package:instant_project/features/calls/constants.dart';
+// import 'package:instant_project/features/calls/doctor%20calls/data/models/call_details_model.dart';
+// import 'package:instant_project/features/calls/helper.dart';
+
+// // Future<List<dynamic>> showCall({required String id})async{
+// //     List<dynamic> data = await Api().get(
+// //       url:'$getCallsApi/$id', 
+// //       token: receptionistToken);
+// //     return data;
+// //   }
+
+// Future <CallDetailsModel> showCallsDetails({required int id, required String token}) async {
+//   final response = await Api().get(
+//     url: '$callsApi/$id',
+//     //token: doctorToken,
+//     token: token
+//   );
+
+//   if (response['message'] == "Success Request" ) {
+//     return CallDetailsModel.fromJson(response['data']) ;
+//   } else {
+//     throw Exception('Error: ${response['message']}');
+//   }
+// }
+
 import 'package:instant_project/features/calls/constants.dart';
 import 'package:instant_project/features/calls/doctor%20calls/data/models/call_details_model.dart';
 import 'package:instant_project/features/calls/helper.dart';
