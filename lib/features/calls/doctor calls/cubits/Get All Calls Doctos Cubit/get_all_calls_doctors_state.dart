0a5bc745b529fe3c468f8ae83eee8510b30c9997
ZeinabAhmed03 <125619131+ZeinabAhmed03@users.noreply.github.com>
2025-03-03 02:51:05 +0200

    Update get_all_calls_doctors_state.dart

--- lib/features/calls/doctor calls/cubits/Get All Calls Doctos Cubit/get_all_calls_doctors_state.dart
+++ lib/features/calls/doctor calls/cubits/Get All Calls Doctos Cubit/get_all_calls_doctors_state.dart
@@ -1,4 +1,22 @@
 
+// import 'package:flutter/material.dart';
+// import 'package:instant_project/features/calls/specialist%20calls/data/models/Card_model.dart';
+
+// @immutable
+// sealed class GetAllCallsDoctorsState {}
+
+// final class GetAllCallsDoctorsInitial extends GetAllCallsDoctorsState {}
+// final class GetAllCallsDoctorsLoading extends GetAllCallsDoctorsState {}
+// final class GetAllCallsDoctorsLoaded extends GetAllCallsDoctorsState {
+//   final List<CardModel> calls;
+//   GetAllCallsDoctorsLoaded({required this.calls});
+  
+// }
+// final class GetAllCallsDoctorsErorr extends GetAllCallsDoctorsState {
+//  final String errorMessage = 'Erorr Loading Calls';
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:instant_project/features/calls/specialist%20calls/data/models/Card_model.dart';
 
@@ -15,3 +33,4 @@ final class GetAllCallsDoctorsLoaded extends GetAllCallsDoctorsState {
 final class GetAllCallsDoctorsErorr extends GetAllCallsDoctorsState {
  final String errorMessage = 'Erorr Loading Calls';
 }
+
