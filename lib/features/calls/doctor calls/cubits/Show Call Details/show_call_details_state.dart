0fe05d0bbdda73646c136492bf298b3f4ccc07ef
ZeinabAhmed03 <125619131+ZeinabAhmed03@users.noreply.github.com>
2025-03-03 02:52:07 +0200

    Update show_call_details_state.dart

--- lib/features/calls/doctor calls/cubits/Show Call Details/show_call_details_state.dart
+++ lib/features/calls/doctor calls/cubits/Show Call Details/show_call_details_state.dart
@@ -1,3 +1,18 @@
+// part of 'show_call_details_cubit.dart';
+
+// @immutable
+// sealed class ShowCallDetailsState {}
+
+// final class ShowCallDetailsInitial extends ShowCallDetailsState {}
+// final class ShowCallDetailsLoading extends ShowCallDetailsState {}
+// final class ShowCallDetailsLoaded extends ShowCallDetailsState {
+//   final CallDetailsModel callDetails;
+
+//   ShowCallDetailsLoaded({required this.callDetails});
+// }
+// final class ShowCallDetailsError extends ShowCallDetailsState {
+//   final String errorMessage = 'Error Loading Call Details';
+// }
 part of 'show_call_details_cubit.dart';
 
 @immutable
