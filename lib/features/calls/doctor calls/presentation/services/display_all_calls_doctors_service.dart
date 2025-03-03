07f0907b2d3cdc475ed9964a9022e22a42ac6189
ZeinabAhmed03 <125619131+ZeinabAhmed03@users.noreply.github.com>
2025-03-03 02:54:09 +0200

    Update display_all_calls_doctors_service.dart

--- lib/features/calls/doctor calls/presentation/services/display_all_calls_doctors_service.dart
+++ lib/features/calls/doctor calls/presentation/services/display_all_calls_doctors_service.dart
@@ -1,18 +1,35 @@
-import 'dart:developer';
+// import 'dart:developer';
 
-import 'package:instant_project/features/calls/helper.dart';
-import 'package:instant_project/features/calls/constants.dart';
-import 'package:instant_project/features/calls/specialist%20calls/data/models/Card_model.dart';
+// import 'package:instant_project/features/calls/helper.dart';
+// import 'package:instant_project/features/calls/constants.dart';
+// import 'package:instant_project/features/calls/specialist%20calls/data/models/Card_model.dart';
 
-// Future<List<CardModel>> displayAllCalls()async{
-//     List<CardModel> data = await Api().get(
-//       url: getCallsApi, 
-//       token: receptionistToken);
+// // Future<List<CardModel>> displayAllCalls()async{
+// //     List<CardModel> data = await Api().get(
+// //       url: getCallsApi, 
+// //       token: receptionistToken);
      
-//     return data;
+// //     return data;
 
-//   }
+// //   }
+// // Future<List<CardModel>> displayAllCallsDoctors() async {
+// //   final response = await Api().get(
+// //     url: callsApi,
+// //     token: doctorToken,
+// //   );
+
+// //   if (response['message'] == "Success Request" && response['data'] is List) {
+// //     return (response['data'] as List)
+// //         .map((call) => CardModel.fromJson(call))
+// //         .toList();
+// //   } else {
+// //     throw Exception('Error: ${response['message']}');
+// //   }
+// // }
+
 // Future<List<CardModel>> displayAllCallsDoctors() async {
+//   log('displayAllCallsDoctors called');
+
 //   final response = await Api().get(
 //     url: callsApi,
 //     token: doctorToken,
@@ -26,6 +43,13 @@ import 'package:instant_project/features/calls/specialist%20calls/data/models/Ca
 //     throw Exception('Error: ${response['message']}');
 //   }
 // }
+
+import 'dart:developer';
+
+import 'package:instant_project/features/calls/helper.dart';
+import 'package:instant_project/features/calls/constants.dart';
+import 'package:instant_project/features/calls/specialist%20calls/data/models/Card_model.dart';
+
 
 Future<List<CardModel>> displayAllCallsDoctors() async {
   log('displayAllCallsDoctors called');
